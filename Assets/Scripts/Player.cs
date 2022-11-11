e92487861c7830455266ffeb1d34a530cd0e1fe3
jongs96 <jjong9696@gmail.com>
2022-11-11 12:18:11 +0900

    charactor ani
    walk blender tree

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : CharacterProperty
+{
+    Vector2 desireDir = Vector2.zero;
+    Vector2 curDir = Vector2.zero;
+    // Start is called before the first frame update
+    enum STATE
+    {
+        Create, Normal, Battle, Death
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) myAnim.SetBool("IsMoving", true);
+        else if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) myAnim.SetBool("IsMoving", false);
+        desireDir.x = Input.GetAxis("Horizontal");
+        desireDir.y = Input.GetAxis("Vertical");
+
+        curDir.x = Mathf.Lerp(curDir.x, desireDir.x, Time.deltaTime * 10.0f);
+        curDir.y = Mathf.Lerp(curDir.y, desireDir.y, Time.deltaTime * 10.0f);
+
+        myAnim.SetFloat("x", curDir.x);
+        myAnim.SetFloat("y", curDir.y);
+    }
+}
