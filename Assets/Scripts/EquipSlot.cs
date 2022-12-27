a9784684c4f89c23e3a7e5934f89c60aa93dd0c3
jongs96 <jjong9696@gmail.com>
2022-12-27 09:33:00 +0900

    d
    d

--- Assets/Scripts/EquipSlot.cs
+++ Assets/Scripts/EquipSlot.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class EquipSlot : MonoBehaviour,IDropHandler,IPointerClickHandler
 {
     public ItemInfo itemInfo;
     public void OnDrop(PointerEventData eventData)
     {
-        if(eventData.pointerDrag.GetComponent<Item>()!=null)
+        Item it = eventData.pointerDrag.GetComponent<Item>();
+        if(it !=null)
         {
-            
+            itemInfo = it.iteminfo;
+            GameObject obj = Instantiate(Resources.Load("Item/EquipItem"), transform) as GameObject;
+            obj.GetComponent<Image>().sprite = Resources.Load("Sprite/" + itemInfo.ItemName) as Sprite;
+            DataManager.Inst.PlayerStatData.AttackPower += itemInfo.Value;
+            UIManager.Inst.SetAbility("AttackPower");
         }
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-        
+        if(eventData.clickCount >=2)
+        {
+
+        }
     }
     // Start is called before the first frame update
     void Start()
